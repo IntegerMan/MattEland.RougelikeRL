04e1c77bd63b4e1f24eb70ba0f571b5eb314f8f2
Matt Eland <matt.eland@gmail.com>
2022-01-15 23:12:40 -0500

    Code Analysis work and early tests

---
+++ MattEland.RoguelikeRL.Logic/NetworkNode.cs
@@ -0,0 +1,9 @@
+﻿namespace MattEland.RoguelikeRL.Logic;
+
+/// <summary>
+/// Represents a node inside of a given <see cref="Network"/>. This is an area that the player will be located in and can move to other nodes.
+/// </summary>
+public sealed class NetworkNode
+{
+    public string Name { get; set; } = "Unknown";
+}
---
+++ MattEland.RoguelikeRL.Tests/NetworkTests.cs
@@ -0,0 +1,48 @@
+using System;
+using MattEland.RoguelikeRL.Logic;
+using NUnit.Framework;
+using Shouldly;
+
+namespace MattEland.RoguelikeRL.Tests
+{
+    public sealed class NetworkTests
+    {
+        [Test]
+        public void NetworksShouldStartEmpty()
+        {
+            // Arrange
+            Network network = new();
+
+            // Act
+            var nodes = network.Nodes;
+
+            // Assert
+            nodes.ShouldBeEmpty();
+            network.NumNodes.ShouldBe(0);
+        }
+
+        [Test]
+        public void AddingANodeShouldIncreaseNodeCount()
+        {
+            // Arrange
+            Network network = new();
+            NetworkNode node = new();
+
+            // Act
+            network.Add(node);
+
+            // Assert
+            network.NumNodes.ShouldBe(1);
+        }
+
+        [Test]
+        public void AddingAnEmptyNodeShouldError()
+        {
+            // Arrange
+            Network net = new();
+
+            // Act / Assert
+            Should.Throw<ArgumentNullException>(() => net.Add(null!));
+        }
+    }
+}
