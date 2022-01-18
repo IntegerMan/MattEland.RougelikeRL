bb3576ce25a7509d2b49d9b727c867345a3a12f7
Matt Eland <matt.eland@gmail.com>
2022-01-17 23:20:06 -0500

    World object

---
+++ MattEland.RoguelikeRL.Tests/GameSessionTests.cs
@@ -0,0 +1,45 @@
+using System;
+using MattEland.RoguelikeRL.Logic;
+using NUnit.Framework;
+using Shouldly;
+
+namespace MattEland.RoguelikeRL.Tests;
+
+public sealed class GameSessionTests
+{
+    [Test]
+    public void GameSessionsShouldStartInTheActiveState()
+    {
+        // Arrange
+
+        // Act
+        GameSession session = new();
+
+        // Assert
+        session.Status.ShouldBe(SessionStatus.Active);
+    }
+
+    [Test]
+    public void GameSessionShouldHaveANetwork()
+    {
+        // Arrange
+
+        // Act
+        GameSession session = new();
+
+        // Assert
+        session.ActiveNetwork.ShouldNotBeNull();
+    }
+
+    [Test]
+    public void GameSessionShouldHaveNetworkNodes()
+    {
+        // Arrange
+
+        // Act
+        GameSession session = new();
+
+        // Assert
+        session.ActiveNetwork.NumNodes.ShouldBeGreaterThan(0);
+    }
+}
